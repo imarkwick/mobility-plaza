d23b5d7b471615d2d568ccd624492a1d6dd574db
imarkwick <izzymarkwick@gmail.com>
2015-11-03 17:39:01 +0000

    links to click through to category show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+category_list = ["Hi-top", "Low-top", "Sporty", "Canvas", "Designer"]
+
+category_list.each do |name|
+  Category.find_or_create_by(name: name)
+end
--- spec/features/category_feature_spec.rb
+++ spec/features/category_feature_spec.rb
@@ -3,14 +3,23 @@ require 'rails_helper'
 RSpec.describe 'Categories: ', type: :feature do
   
   before do
-    Category.find_or_create_by(name: 'Hi-top')
+    @hi_top = Category.find_or_create_by(name: 'Hi-top')
   end
 
   context 'exist on Fresh Creps' do
+    
     scenario 'and are displayed on the home page' do
       visit '/categories'
       expect(page).to have_content 'Hi-top'
     end
+
+    scenario 'link through to the individual product page' do
+      category_url = '/categories/' + @hi_top.id.to_s
+      visit '/categories'
+      click_link 'Hi-top'
+      expect(current_path).to eq category_url
+    end
+
   end
 
 end
