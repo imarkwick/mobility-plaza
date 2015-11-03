e3c3d6ed471eb0795ceb26e5369911ecb458f8fd
imarkwick <izzymarkwick@gmail.com>
2015-11-03 16:51:59 +0000

    category model created

--- spec/features/category_feature_spec.rb
+++ spec/features/category_feature_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Categories: ', type: :feature do
   
   before do
-
+    Category.find_or_create_by(name: 'Hi-top')
   end
 
   context 'exist on Fresh Creps' do
