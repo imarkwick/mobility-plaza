d68490742a195170b80b941c41746d22f94db691
imarkwick <izzymarkwick@gmail.com>
2015-11-12 13:14:16 +0000

    testing trainer colour join model

--- spec/models/colour_spec.rb
+++ spec/models/colour_spec.rb
@@ -2,29 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Colour do 
 
-  # before do
-  #   @orange = Colour.create(name: 'Orange')
-  #   @trainer = Trainer.create(name: 'Air Max 90')
-  #   @trainer2 = Trainer.create(name: 'Air Force One')
-  # end
+  before do
+    @orange = Colour.create(name: 'Orange')
+    @trainer = Trainer.create(name: 'Air Max 90')
+    @trainer2 = Trainer.create(name: 'Air Force One')
+  end
 
-  # it 'should be valid' do
-  #   expect(@orange.valid?).to be true
-  # end
+  it 'should be valid' do
+    expect(@orange.valid?).to be true
+  end
 
-  # it 'should have a name' do
-  #   expect(@orange.name).to eq 'Orange'
-  # end
+  it 'should have a name' do
+    expect(@orange.name).to eq 'Orange'
+  end
 
-  # it 'can have a trainer that features the colour' do
-  #   @trainer.update(colour: @orange)
-  #   expect(@trainer.colour.name).to eq 'Orange'
-  # end
+  it 'can have a trainer' do
+    @trainer.colours = [@orange]
+    expect(@trainer.colours.first.name).to eq 'Orange'
+  end
 
-  # it 'can have multiple trainers with that colour' do
-  #   @trainer.update(colour: @orange)
-  #   @trainer2.update(colour: @orange)
-  #   expect(@orange.trainers.count).to eq 2
-  # end
+  it 'can have multiple trainers with that colour' do
+    @trainer.update(colours: [@orange])
+    @trainer2.update(colours: [@orange])
+    expect(@orange.trainers.count).to eq 2
+  end
   
 end
