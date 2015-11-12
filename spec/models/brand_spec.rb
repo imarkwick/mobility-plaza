23007833d6dc3471f3ec008725b17cfa69a04430
imarkwick <izzymarkwick@gmail.com>
2015-11-12 12:02:25 +0000

    testing brand model

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -1,1 +1,30 @@
 require 'rails_helper'
+
+RSpec.describe Brand do 
+
+  before do 
+    @brand = Brand.create(name: 'Nike')
+    @trainer = Trainer.create(name: 'Air Max 90')
+    @trainer2 = Trainer.create(name: 'Air Force One')
+  end
+
+  it 'should be valid' do
+    expect(@brand.valid?).to be true
+  end
+
+  it 'should have a name' do
+    expect(@brand.name).to eq 'Nike'
+  end
+
+  it 'can have a trainer' do
+    @trainer.update(brand: @brand)
+    expect(@trainer.brand.name).to eq 'Nike'
+  end
+
+  it 'can have multiple trainers' do
+    @trainer.update(brand: @brand)
+    @trainer2.update(brand: @brand)
+    expect(@brand.trainers.count).to eq 2
+  end
+
+end
