53b427cb67e7c6995ef1e384d6c1b77b3cfeaae4
imarkwick <izzymarkwick@gmail.com>
2016-01-05 12:00:15 +0000

    i can't even remember what was last changed

--- spec/features/trainer_feature_spec.rb
+++ spec/features/trainer_feature_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Trainers: ', type: :feature do 
 
+  context 'featured on Fresh Creps' do
+
+    scenario 'and are sorted into categories' do
+
+    end
+
+  end
+
+
 end
